2ffb9559ecf37e2c101cd459ce72330818b33891
Jasim A Basheer <jasim.ab@gmail.com>
2013-09-03 11:55:59 +0530

    Introduce zeus. Aren't tests better than REPL now?
    Use script/startzeus to start the server for use with RubyMine.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,9 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-require 'rspec/autorun'
+
+# Zeus runs the tests twice with autorun. https://github.com/burke/zeus/issues/180
+# require 'rspec/autorun'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -39,4 +41,13 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+end
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/cassettes'
+  config.hook_into :webmock
+  config.configure_rspec_metadata!
+  config.default_cassette_options = {
+      re_record_interval: 1.week
+  }
 end
